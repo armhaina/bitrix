d3aa169876f1779277c7d64fe97bfe46712cba02
Георгий Либиков <armhaina@mail.ru>
2026-02-05 20:31:31 +0300

    app

---
+++ php-8.5-fpm-trixie/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source /tmp/scr/step-0-text-decoration.sh
+source /tmp/scr/step-1-variables-declaring.sh
+source /tmp/scr/step-2-composer-install.sh
+source /tmp/scr/step-3-variables-change.sh
+source /tmp/scr/step-4-nginx-conf-def-disable.sh
+source /tmp/scr/step-5-bitrix-install.sh
+source /tmp/scr/step-6-bitrix-cache.sh
+source /tmp/scr/step-final-supervisor-run.sh
